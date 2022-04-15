7fb899d56b6cf054350ef063ede884de94925ee6
Alex Batenhorst <alexfbaten@gmail.com>
2022-04-15 10:43:53 -0500

    Adj."shift tab," completed console items list O-Q.

--- reviewsEats/Program.cs
+++ reviewsEats/Program.cs
@@ -4,43 +4,56 @@ namespace HelloWorld
 {
   class Program
   {
-    static void Main(string[] args)
-    {
-        bool userChoice;
-        string userChoiceString;
-        string[] eateryArray = new string[10]; //reservce 10 spaces to hold string (memory)
-   //Repeat main loop  
-    do
-    {
+        static void Main(string[] args)
+        {
+            bool userChoice;
+            string userChoiceString;
+            string[] eateryArray = new string[10]; //reservce 10 spaces to hold string (memory)
+    //Repeat main loop  
         do
         {
-        //Initialize variables
-        userChoice = false;
-        //   TODO: Provide the user a menu of options
-        Console.WriteLine("Please enter your Good Eat's review (0-5): ");
+            do
+            {
+            //Initialize variables
+            userChoice = false;
+            //   TODO: Provide the user a menu of options
+            Console.WriteLine("Please enter your Good Eat's review (0-5): ");
+            Console.WriteLine("O - Open the user's list of restaurants, ");// user array (Alec's notes)
+            Console.WriteLine("S - Save the user's list of restaurants");// save the array to the data file 
+            Console.WriteLine("C - Add a restaurant and rating, ");//add a name to the array 
+            Console.WriteLine("R - Print a list of all the restaurants and their rating, ");//Read a name from the array. {end here for the comp. check}
+            Console.WriteLine("U - Update the rating for a restaurant, ");//Update a name in the array
+            Console.WriteLine("D - Delete a restaurant");//Delete a name from the array.
+            Console.WriteLine("Q: Quit the program.");// (last item for comp check)
 
-        userChoiceString = Console.ReadLine();
 
-        userChoice =(userChoiceString == "A" ||  userChoiceString=="a" ||
-                    userChoiceString == "B" ||  userChoiceString=="b" );
-    
+            userChoiceString = Console.ReadLine();
 
-        if (!userChoice)
-        {
-            Console.WriteLine("Please enter a valid option");
-        }
-
-        }//ends 2nd "do" 
+            userChoice =(userChoiceString == "O" ||  userChoiceString== "o" ||//
+                        userChoiceString == "S" ||  userChoiceString== "s" ||//
+                        userChoiceString == "C" || userChoiceString == "c" ||//
+                        userChoiceString == "R" || userChoiceString == "r" ||//
+                        userChoiceString == "U" ||  userChoiceString== "u" ||
+                        userChoiceString == "D" || userChoiceString == "d" || 
+                        userChoiceString == "Q" || userChoiceString == "q");//   
         
-        while(!userChoice);
-    
-    }//ends the big "do" 
-    
-    while(!(userChoiceString=="Q"));    
-    
-    
-    }//ends static main  
 
+            if (!userChoice)
+            {
+                Console.WriteLine("Please enter a valid option");
+            }
 
-  }//main program 
-}
+            }//ends 2nd "do" 
+            
+            while(!userChoice);
+        
+        }//ends the big "do" 
+        
+        while(!(userChoiceString=="Q"));    
+    
+    
+    }//ends static void   
+
+  }//class program  
+
+}//name space 
