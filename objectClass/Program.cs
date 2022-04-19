bdbbf3a9df296d63f0ed6115459239339ef88bff
Alex Batenhorst <alexfbaten@gmail.com>
2022-04-19 13:54:56 -0500

    getting template ready

--- objectClass/Program.cs
+++ objectClass/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 
-namespace konomiApp // Note: actual namespace depends on the project name.
+namespace konomiAPP
 {
-    class Program
-    {
-        static void MainWindow(string[] args)
+  class Program
+  {
+    static void Main(string[] args)
+    { 
+        // Declare and instantiate the array of song objects
+        DDR[] songArray=new DDR[25];
+
+        // Now, loop through each array element and instantiate a Song object for each.
+        // Note that the constructor with no parameters will be used.
+
+        for (int index = 0; index < songArray.Length; index++)
         {
-            Console.WriteLine("Hello World!");
+            songArray[index] = new DDR();
         }
-    }
-}
+        // create two data values 
+        //data value 1:
+        songArray[1].setSongName("Butterfly");
+        //data value 2: difficulty level
+        // print each restaurant to test the property gets and the toString
+        for (int index = 0; index < songArray.Length; index++)
+        {
+            if (!(((songArray[index]).getName())==null))
+                Console.WriteLine(songArray[index]);
+        }
+
+    } // end Main
+  } // end class
+} // namespace
