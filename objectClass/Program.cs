8aff3e51edb4a1b18cc91ac743f5aefd8c17f772
Alex Batenhorst <alexfbaten@gmail.com>
2022-04-19 10:14:44 -0500

    Setting up cs files

---
+++ objectClass/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace konomiApp // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ objectClass/konamiClass.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace konomiApp // Note: actual namespace depends on the project name.
+{
+    class konomiClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
