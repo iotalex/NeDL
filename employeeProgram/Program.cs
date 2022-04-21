b006f800ebfb6a0ba456990b4a81229ff9fcb13d
Alex Batenhorst <alexfbaten@gmail.com>
2022-04-21 16:57:16 -0500

    added hourly type

--- employeeProgram/Program.cs
+++ employeeProgram/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
-
+/*
+	(Start here) 1. R - Print a list of all the employees including their calculated bonus, (r will print employee and show calculate bonus; 
+create a bonus calculate function—use polymorphism [two string]); will call depending on type of employee they are. 
+*/
 namespace MyWorkApplication
   {
     class Program
@@ -11,17 +14,21 @@ namespace MyWorkApplication
         string getFirstName = "John";
         char firstName;//declares type 
         string getTypeSalary = "Salary";
+
+        string getTypeHourly = "Hourly";
       static void Main(string[] args)      
           // public virtual void hourlyRate()
         {
           Employee lastName = new Employee(); //last name object 
           Employee firstName = new Employee(); //first name object 
-          Employee typeSalary = new Employee(); //salary type object 
+          Employee typeSalary = new Employee(); //salary type object
+          Employee typeHourly = new Employee(); //hourly type object 
         
           //outputs the lines using objects
           Console.WriteLine("Employee's Last name: " + lastName.getLastName);
           Console.WriteLine("First name: " + firstName.getFirstName);
-          Console.WriteLine("Reinbursement type: " + typeSalary.getTypeSalary);
+          Console.WriteLine("Reinbursement salary: " + typeSalary.getTypeSalary);
+          Console.WriteLine("Reinbursement hourly: " + typeHourly.getTypeHourly);
         }
       }
  
