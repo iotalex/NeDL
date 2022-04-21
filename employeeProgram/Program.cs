750f360cd182ede5f62d55f440bd6cd5219d5ba0
Alex Batenhorst <alexfbaten@gmail.com>
2022-04-21 11:08:33 -0500

    program template, begining, add class objects

--- employeeProgram/Program.cs
+++ employeeProgram/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
 
-namespace HelloWorld
+namespace MyWorkApplication
 {
+  class Employee  // Base class (parent) 
+  {
+    public virtual void hourlyRate()
+    {
+      Console.WriteLine("These are the pay types: ");
+    }
+  }
+
+  class Hourly : Employee  // Derived class (child) 
+  {
+    public override void hourlyRate()
+    {
+      Console.WriteLine("$35.50 hr");
+    }
+  }
+
+  class Salary : Employee  // Derived class (child) 
+  {
+    public override void hourlyRate()
+    {
+      Console.WriteLine("$65k a year");
+    }
+  }
+
   class Program
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");  
+      Employee myEmployee = new Employee();  // Create an Employee object
+      Employee myHourly = new Hourly();  // Create a Hourly rate object
+      Employee mySalary = new Salary(); //Create a salary rate object 
+      
+      myEmployee.hourlyRate();
+      myHourly.hourlyRate();
+      mySalary.hourlyRate();
+      //myPig.animalSound();
+      //myDog.animalSound();
     }
   }
 }
