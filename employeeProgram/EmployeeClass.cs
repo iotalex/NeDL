06e070a61ff7e1dbd5891d9c4da28a4ef4882612
Alex Batenhorst <alexfbaten@gmail.com>
2022-04-21 16:12:54 -0500

    not sure what's going on, but it's working

--- employeeProgram/EmployeeClass.cs
+++ employeeProgram/EmployeeClass.cs
@@ -4,30 +4,22 @@ namespace MyWorkApplication
 {
     class Employee  // Base class (parent object) 
   {
-    string getLastName = "Smith";
-    char lastName; //declares type
-    string getFirstName = "John";
-    char firstName;//declares type 
-    string getTypeSalary = "Salary";
+         
+    public class HourlyEmployee : Employee  // Derived class (child object) 
+      {
+        
+      }
+    private class SalaryEmployee : Employee  // Derived class (child object) 
+      {
+        
+      }
 
-    static void Main(string[] args)      
-       // public virtual void hourlyRate()
-    {
-      Employee lastName = new Employee(); //last name object 
-      Employee firstName = new Employee(); //first name object 
-      Employee typeSalary = new Employee(); //salary type object 
-    
-      //outputs the lines using objects
-      Console.WriteLine("Employee's Last name: " + lastName.getLastName);
-      Console.WriteLine("First name: " + firstName.getFirstName);
-      Console.WriteLine("Reinbursement type: " + typeSalary.getTypeSalary);
-     
-    
-    }
-
+  
     
     
        
 
   }//end class employee
+
+  
 }
--- employeeProgram/Program.cs
+++ employeeProgram/Program.cs
@@ -1,20 +1,33 @@
 ﻿using System;
 
 namespace MyWorkApplication
-{
-  class Program
   {
-    public class HourlyEmployee : Employee  // Derived class (child object) 
+    class Program
+    {
+      class Employee  // Base class (parent object) 
       {
+        string getLastName = "Smith";
+        char lastName; //declares type
+        string getFirstName = "John";
+        char firstName;//declares type 
+        string getTypeSalary = "Salary";
+      static void Main(string[] args)      
+          // public virtual void hourlyRate()
+        {
+          Employee lastName = new Employee(); //last name object 
+          Employee firstName = new Employee(); //first name object 
+          Employee typeSalary = new Employee(); //salary type object 
         
+          //outputs the lines using objects
+          Console.WriteLine("Employee's Last name: " + lastName.getLastName);
+          Console.WriteLine("First name: " + firstName.getFirstName);
+          Console.WriteLine("Reinbursement type: " + typeSalary.getTypeSalary);
+        }
       }
-    private class SalaryEmployee : Employee  // Derived class (child object) 
-      {
-        
-      }
+ 
+    }//end class
+}//end main 
 
-  }
-}
 
 
 
