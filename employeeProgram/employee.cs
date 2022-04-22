db2836e9c91be476682838676ab7c0af317a0db1
Alex Batenhorst <alexfbaten@gmail.com>
2022-04-22 11:36:31 -0500

    refinces strings, 1. R, strings

--- employeeProgram/employee.cs
+++ employeeProgram/employee.cs
@@ -12,22 +12,23 @@ namespace MyWorkApplication
             char lastName; //declares type?
             string getFirstName = "John";
             char firstName;//declares type?
-            string getTypeSalary = "Salary";
+            string getTypeSalary = "Salary || hourly?";
 
-            string getTypeHourly = "Hourly";
+            string getTypeBonus = "Calcuated bonus: goes here $$$";
         static void Main(string[] args)      
             // public virtual void hourlyRate()
             {
             Employee lastName = new Employee(); //last name object
             Employee firstName = new Employee(); //first name object 
             Employee typeSalary = new Employee(); //salary type object
-            Employee typeHourly = new Employee(); //hourly type object 
+            Employee typeBonus = new Employee(); //hourly type object 
             
             //outputs the lines using objects
-            Console.WriteLine("Employee's Last name: " + lastName.getLastName);
-            Console.WriteLine("First name: " + firstName.getFirstName);
-            Console.WriteLine("Reinbursement salary: " + typeSalary.getTypeSalary);
-            Console.WriteLine("Reinbursement hourly: " + typeHourly.getTypeHourly);
+            Console.WriteLine("Reinbursement hourly: " + typeBonus.getTypeBonus);
+            Console.WriteLine("Emplyee name: " + lastName.getLastName + ", " + firstName.getFirstName);
+            Console.WriteLine("Reinbursement type: " + typeSalary.getTypeSalary);
+            Console.WriteLine("Bonus: " + typeBonus.getTypeBonus); 
+            
             }
         }
         
