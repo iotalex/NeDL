f20843295dacdfd9eb9b9250fe5a560ba866ee11
Alex Batenhorst <alexfbaten@gmail.com>
2022-04-26 13:28:28 -0500

    start template for/create a list

---
+++ ObjectClassList/DDRSong.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace Stepmania // Note: actual namespace depends on the project name.
+{
+     class DDRSong //abstract class 
+    {
+        public string songName
+            {get; set;}
+        int footer
+            {get; set;}
+        public string color
+            {get; set;}
+        
+        public DDRSong()
+        {
+            songName = " ";
+            footer = 0;
+            color = " "; 
+        }
+
+        public DDRSong(string newSongName, int newFooter, string newColor)//default constructor 
+        {
+            songName = newSongName;
+            footer = newFooter;
+            color = newColor;
+        }
+        
+        public override string ToString()
+        {
+            string outputString = $"Song Name: {songName} | Footer: {footer} | Series: {color}";
+            return outputString;
+        }
+
+    }//end class song
+
+}//end name space
---
+++ ObjectClassList/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+//TODO: add something here for the list 
+
+namespace Stepmania // Note: actual namespace depends on the project name.
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine ("Lets dance!");
+        //array of DDRSong's  
+        DDRSong[] ddrSongArray =new DDRSong[25];
+        AceDDRSong[] ddrAceSongArray = new AceDDRSong[25];
+        
+        //now load in some test data 
+        AceDDRSong newExtremeSong = new AceDDRSong("Butterfly", 9, "Extreme", "Purple");// DDR object 
+        DDRSong newDDRAceSong = new DDRSong("I'm an Albatraoz", 11, "Ace");// DDR object
+        //now print object
+        Console.WriteLine(newExtremeSong);
+        Console.WriteLine(newDDRAceSong);
+
+        ddrSongArray[1] = newExtremeSong; //main object 
+        ddrSongArray[2] = newDDRAceSong; //main object 
+
+        Console.WriteLine("Print the array: ");
+        
+        //TODO: get item to print 
+
+
+
+
+        }
+    }//end class 
+}//end name space 
