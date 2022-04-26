0f629114f15582add208741f820665dabe8ba01e
Alex Batenhorst <alexfbaten@gmail.com>
2022-04-26 09:28:50 -0500

    getters-setters

---
+++ ObjectClassAbstract2/DDRSong.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace Stepmania // Note: actual namespace depends on the project name.
+{
+     class DDRSong
+    {
+        public string songName
+            {get; set;}
+        int footer
+            {get; set;}
+        public string seriesType
+            {get; set;}
+        
+        
+        public DDRSong()
+        {
+            songName = " ";
+            footer = 0;
+            seriesType = " "; 
+
+        }
+
+        public DDRSong(string newSongName, int newFooter, string newSeriesType)//default constructor 
+        {
+            songName = newSongName;
+            footer = newFooter;
+            seriesType = newSeriesType;
+        }
+
+    }//end class song 
+
+}//end name space
