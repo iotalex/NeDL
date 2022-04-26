dc6fa82264ddbb358887de08dca7f565dc6838aa
Alex Batenhorst <alexfbaten@gmail.com>
2022-04-26 09:28:30 -0500

    main parent class created, getters/setters

--- ObjectClassAbstract2/Program.cs
+++ ObjectClassAbstract2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+//TODO: add something here for the list 
+
+namespace Stepmania // Note: actual namespace depends on the project name.
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            class DDRSong[] = ddrSongArray = new DDRSong[25];//array of songs
+        }
+    }//end class 
+}//end name space 
