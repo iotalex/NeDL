c9de78383d94ef42ae679c0609fcc3f321cc9531
Alex Batenhorst <alexfbaten@gmail.com>
2022-04-26 10:09:33 -0500

    creating new child element, AceDDR

--- ObjectClassAbstract2/ExtremeDDRSong.cs
+++ ObjectClassAbstract2/ExtremeDDRSong.cs
@@ -5,17 +5,23 @@ namespace Stepmania // Note: actual namespace depends on the project name.
 {
      class ExtremeDDRSong : DDRSong
     {
-    public string extremeSeries
-        {get; set;}
-    public ExtremeDDRSong() : base()//default constructor
-    {
-        extremeSeries = " ";
-    }
+        public string extremeSeries
+            {get; set;}
+        
+        public ExtremeDDRSong() : base()//default constructor
+        {
+            extremeSeries = " ";
+        }
 
-    public ExtremeDDRSong(string newName, int newFooter, string newSeriesType, string newExtremeSeries) : base(newName, newFooter, newSeriesType)
-    {
-        extremeSeries = newExtremeSeries;
-    }
+        public ExtremeDDRSong(string newName, int newFooter, string newSeriesType, string newExtremeSeries) : base(newName, newFooter, newSeriesType)
+        {
+            extremeSeries = newExtremeSeries;
+        }
+        
+        public override string ToString()
+        {
+            return (base.ToString() + " | " + "Difficulty: " + extremeSeries);
+        }
 
     }//end class song
 
