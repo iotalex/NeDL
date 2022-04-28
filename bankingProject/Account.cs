f0bb0309eada0c5dda8042bac5a5912db8ffbd24
Alex Batenhorst <alexfbaten@gmail.com>
2022-04-28 10:38:56 -0500

    pseudo code parent class

--- bankingProject/Account.cs
+++ bankingProject/Account.cs
@@ -2,9 +2,32 @@
 
 namespace BankingApp 
 {
-    internal class Account
-    {
-        
+    
+    class Account{
+        //strings getters/setters
+        //account id, the type of account, and current balance.
+        //get accountId
+
+        //get type of accountType
+
+        //get currentBalance
+
+
+        //contructors
+        public Account(){
+            //types from above
+        }
+        //default constructor strings 
+        //public Account(string );
+
+        //method(s) like 
+
+            //depositmethod
+
+            //abstract withdrawal method
+
+
+            //useful misc method useful to toString 
 
 
 
