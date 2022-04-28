d591f09dfd1864ca001ac95ac9c1d28f3a8b5ad3
Alex Batenhorst <alexfbaten@gmail.com>
2022-04-28 10:17:32 -0500

    beggining template, parent class

---
+++ bankingProject/Account.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BankingApp 
+{
+    internal class Account
+    {
+        class Account{}//end class account
+
+
+
+        
+    }//end class Program 
+}//end namespace
---
+++ bankingProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BankingApp 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+
+        
+    }//end class Program 
+}//end namespace
