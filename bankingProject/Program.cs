6167fe58e1b637b4995476b391ea69229c92ceaa
Alex Batenhorst <alexfbaten@gmail.com>
2022-04-30 11:42:39 -0500

    added transaction menu

--- bankingProject/Program.cs
+++ bankingProject/Program.cs
@@ -43,8 +43,6 @@ namespace BankingApp{
                 Console.WriteLine("W: Make a Withdrawal");
                 Console.WriteLine("P: Pick an account: ");
                 Console.WriteLine("Z: Make a checking transaction");
-               
-
                 //  TODO: Get a user option (valid means its on the menu)
 
                 userChoiceString = Console.ReadLine();
@@ -144,17 +142,98 @@ namespace BankingApp{
             
             else if (userChoiceString=="P" || userChoiceString=="p")
             {
-                Console.WriteLine("List of accounts: ");
-                Console.WriteLine("----------------------------------------------------------------------------------------");
-                foreach(Account anAccountList in accountList)
+                   bool userChoice2;
+                    string userChoiceString2;
+                    
+                //Repeat main loop 
+                    do
+                    {     
+                    //  TODO: Get a valid input 
+                    do
                     {
-                        Console.WriteLine(anAccountList);//prints the list above (test data)
-                    }//end foreach       
+
+                        //Initialize variables
+                        
+                        userChoice2 = false;
+                        
+                        //   TODO: Provide the user a menu of options
+
+                        Console.WriteLine("What's your pleasure?");
+                        Console.WriteLine("C: Checking account.");
+                        Console.WriteLine("S: Savings account");
+                        Console.WriteLine("Cd account: .");
+                        Console.WriteLine("Q: Quit the program.");
+                        
+                            
+                        //   TODO: Get a valid user option (valid means its on the menu)
+                        userChoiceString2 = Console.ReadLine();
+                        
+                        
+                        userChoice2 = (userChoiceString2== "C" || userChoiceString2 == "c" ||  
+                                        userChoiceString2 == "S" || userChoiceString2 == "s" ||  
+                                        userChoiceString2 == "D" || userChoiceString2 == "d" ||      
+                                        userChoiceString2 == "Q" || userChoiceString2 == "q");  
+                                
+                            if(!userChoice2)
+                            {
+                            Console.WriteLine("Please enter a valid option");
+                            }    
+                        }//ends the second "do" which is to get "valid input"
+                        while (!userChoice2);
+
+                    //   Select checking account
+                            if (userChoiceString2=="C" || userChoiceString2=="c")
+                            {
+                                 double withDrawalAmount;
+                int accountId; 
+                Console.WriteLine("=========================CHECKING ACCOUNT=============================");
+                Console.WriteLine("How much would you like to withdrawal?");
+                
+                withDrawalAmount = Convert.ToDouble(Console.ReadLine());
+                while(withDrawalAmount <= 0)
+                    {
+                    Console.WriteLine("-------------Warning-------------");//warns user to enter positive integer
+                    Console.WriteLine("PLEASE ENTER A POSITIVE NUMBER!");//warns user to enter positive integer
+                    if(withDrawalAmount < 0)//second chance to enter positive number
+                        {
+                            withDrawalAmount = Convert.ToDouble(Console.ReadLine());
+                        }
+                    break; 
+                    }//end if
+                Console.WriteLine("Please enter account number"); 
+                accountId = Convert.ToInt32(Console.ReadLine());
+                
+                foreach(Account anAccount in accountList)
+                {
+                    if(anAccount.accountId==accountId)
+                        {
+                            anAccount.MakeWithdrawal(withDrawalAmount);
+                            Console.WriteLine("Withdrawal made");//prints the list above (test data)
+                        }//end if 
+                } 
+                            
+                            }//end using    
+
+                    //   Select savings account
+                            else if (userChoiceString2=="S" || userChoiceString2=="s")
+                            {
+                            //equivilent of saying "CTR + S" 
+                            }
+                    //   Select CD account  
+                            else if (userChoiceString2 =="CD" || userChoiceString2 =="cd") 
+                            {
+                            //typicall if statements 
+                            }
+                    
+                    //   TODO: Else if the option is a Q or q then quit the program                
+                            else if (userChoiceString2=="Q" || userChoiceString2=="q")
+                            {
+                            Console.WriteLine("Goodbye, auf wiedersehen");
+                            }
+                }// ends the big "do"
+                while(!(userChoiceString2=="Q"));
+
             }//end if
-
-
-
-
 
             //savings: withdrawal is allowed but only as long as the balance is great than the withdrawal amount 
 
