e1a96711d988887b41c2cddb95b0c1b89c688ed1
Alex Batenhorst <alexfbaten@gmail.com>
2022-05-02 13:29:27 -0500

    base class created

---
+++ shoppingApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;   // needed for Lists
+
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
