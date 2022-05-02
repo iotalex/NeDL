170c6bb1a15143bd0f7fa65ba9ef8748e23bb91a
Alex Batenhorst <alexfbaten@gmail.com>
2022-05-02 15:02:52 -0500

    base class

--- shoppingApp/Program.cs
+++ shoppingApp/Program.cs
@@ -3,13 +3,27 @@ using System.Collections;
 using System.Collections.Generic;   // needed for Lists
 
 
-namespace MyApp // Note: actual namespace depends on the project name.
+namespace ShopKo // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Create a list of Employees
+        List<Member> memberList = new List<Member>();
+
+        // Add a couple of employees to the list to test
+        memberList.Add(new Member ("Enge", "Alec", "H", 6, 5));
+
+            // Print the list
+            foreach (Member anMember in memberList)
+            {
+                Console.WriteLine(anMember);
+            }  // end foreach  
         }
-    }
-}
+
+
+
+        
+    }//end class
+}//end name space
--- shoppingApp/Shop.cs
+++ shoppingApp/Shop.cs
@@ -1,15 +1,45 @@
 ﻿using System;
 
-namespace MyApp // Note: actual namespace depends on the project name.
+namespace ShopKo // Note: actual namespace depends on the project name.
 {
-    public class Shop
+    class Member //abstract class, objects can't be instantiated (objects of this type can't be created)    
     {
-    
-    
-    
-    
-    
-    
-    }
+        public string memberId
+            {get; set;}
+        public string emailAddress
+            {get; set;}
+        public string membershipType
+            {get; set;}
+        public double annualCost
+            {get; set;}
+        public double currentMonthPurchases
+            {get; set;}
+        
+        //create getters and setters (properties)
+        public Member()
+        {
+            memberId = " "; 
+            emailAddress = " ";
+            membershipType = " ";
+            annualCost = 0.0;
+            currentMonthPurchases = 0.0; 
+        }
+            //constructors  
+        public Member(string newMemberId, string newEmailAddress, string newMembershipType, int newAnnualCost, double newCurrentMonthPurchases)//defualt constructor
+        {
+            memberId = newMemberId;
+            emailAddress = newEmailAddress;
+            membershipType = newMembershipType;
+            annualCost = newAnnualCost;
+            currentMonthPurchases = newCurrentMonthPurchases;
+
+
+        }
+        
+
+
+
+
+    }//end class
 
 }//end main 
