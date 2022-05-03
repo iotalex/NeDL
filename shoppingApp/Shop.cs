f304805435b5a4470925f03a562baee3818b6aa3
Alex Batenhorst <alexfbaten@gmail.com>
2022-05-03 09:55:01 -0500

    made parent abstract class, add children

--- shoppingApp/Shop.cs
+++ shoppingApp/Shop.cs
@@ -2,7 +2,7 @@
 
 namespace ShopKo // Note: actual namespace depends on the project name.
 {
-    class Member //abstract class, objects can't be instantiated (objects of this type can't be created)    
+    abstract class Member //abstract class, objects can't be instantiated (objects of this type can't be created)    
     {
         public string memberId
             {get; set;}
