58541a2c9e099c22d1b14c6bab2f9fdbc6943e16
Alex Batenhorst <alexfbaten@gmail.com>
2022-05-03 11:03:54 -0500

    fixed class type int to int ID from string ID

--- shoppingApp/Regular.cs
+++ shoppingApp/Regular.cs
@@ -4,20 +4,27 @@ namespace ShopKo // Note: actual namespace depends on the project name.
 {
     class Regular : Member //abstract class, objects can't be instantiated (objects of this type can't be created)    
     {
-        //create getters and setters (properties)
-        public Regular()
+        public double cashBack
+            {get; set;}
+        
+        public Regular() : base() //defualt constructor
         {
-            
+            cashBack = 0.0;
         }
             //constructors  
-        public Regular(string newMemberId, string newEmailAddress, string newMembershipType, int newAnnualCost, double newCurrentMonthPurchases) : base(newMemberId, newEmailAddress, newMembershipType, newAnnualCost, newCurrentMonthPurchases) //another constructor
+        public Regular(int newMemberId, string newEmailAddress, string newMembershipType, int newAnnualCost, double newCurrentMonthPurchases, double newcashBack): base(newMemberId, newEmailAddress, newMembershipType, newAnnualCost, newCurrentMonthPurchases) //another constructor
         {
-            
+            cashBack = newcashBack;
+        }
+        //TODO: add implement method
+        public override double ICalculate() // this is the abstract method that all children must implement
+        {
+            return cashBack * .02;
         }
 
         public override string ToString()
         {
-            return$"Member ID#: {memberId} | Email Address: {emailAddress} | Type: {membershipType} | Annual fee: {annualCost} | Monthly Purchases: $ {currentMonthPurchases} "; 
+            return base.ToString() + $" | ????: ${cashBack} | Cashback: {ICalculate()}"; 
         }
         
 
