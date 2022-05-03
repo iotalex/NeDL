8caf9dee614658461f24707e4c045408ba27e011
Alex Batenhorst <alexfbaten@gmail.com>
2022-05-03 10:07:25 -0500

    added child class Regular, prints out test data

--- shoppingApp/Program.cs
+++ shoppingApp/Program.cs
@@ -10,14 +10,19 @@ namespace ShopKo // Note: actual namespace depends on the project name.
         static void Main(string[] args)
         {
             // Create a list of Employees
-        List<Member> memberList = new List<Member>();
+        List<Member> regularMemberList = new List<Member>();
 
         // Add a couple of employees to the list to test
-        memberList.Add(new Member ("Alex", "al@bates.com", "Standard Member", 60, 5));
-        memberList.Add(new Member ("Katie", "woot@aol.org", "Regular Member" , 45, 99));
+        //memberList.Add(new Member ("Alex", "al@bates.com", "Standard Member", 60, 5));
+       //memberList.Add(new Member ("Katie", "woot@aol.org", "Regular Member" , 45, 99));
+
+        regularMemberList.Add(new Regular ("Alex", "al@bates.com", "Regular Member", 60, 5.55));
+
+        regularMemberList.Add(new Regular ("Katie", "woot@aol.org", "Regular Member" , 45, 99));
+
 
             // Print the list
-            foreach (Member anMember in memberList)
+            foreach (Member anMember in regularMemberList)
             {
                 Console.WriteLine(anMember);
             }  // end foreach  
---
+++ shoppingApp/Regular.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ShopKo // Note: actual namespace depends on the project name.
+{
+    class Regular : Member //abstract class, objects can't be instantiated (objects of this type can't be created)    
+    {
+        //create getters and setters (properties)
+        public Regular()
+        {
+            
+        }
+            //constructors  
+        public Regular(string newMemberId, string newEmailAddress, string newMembershipType, int newAnnualCost, double newCurrentMonthPurchases) : base(newMemberId, newEmailAddress, newMembershipType, newAnnualCost, newCurrentMonthPurchases) //another constructor
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            return$"Member ID#: {memberId} | Email Address: {emailAddress} | Type: {membershipType} | Annual fee: {annualCost} | Monthly Purchases: $ {currentMonthPurchases} "; 
+        }
+        
+
+    }//end class
+
+}//end main 
