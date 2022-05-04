69d0d6b2b2234b0f73e0860d21348cd50195db15
Alex Batenhorst <alexfbaten@gmail.com>
2022-05-04 09:41:20 -0500

    class new created, remove new cashback

---
+++ shoppingApp/NonProfit.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ShopKo // Note: actual namespace depends on the project name.
+{
+    class NonProfit : Member, IGetSpecialOffer //abstract class, objects can't be instantiated (objects of this type can't be created)    
+    {
+        public double cashBack
+            {get; set;}
+        
+        public NonProfit() : base() //defualt constructor
+        {
+            cashBack = 0.0;
+        }
+            //constructors  
+        public NonProfit(int newMemberId, string newEmailAddress, string newMembershipType, int newAnnualCost, double newCurrentMonthPurchases, double newCashBack): base(newMemberId, newEmailAddress, newMembershipType, newAnnualCost, newCurrentMonthPurchases) //another constructor
+        {
+            cashBack = newCashBack;
+        }
+        //TODO: add implement method
+        public double GetSpecialOffer()//interface method
+        {
+            return annualCost * .50;
+        }
+     
+     
+        public override string ToString()
+        {
+            return base.ToString() + $" || Cashback: {GetCashBack(currentMonthPurchases)} Special Offer: ${GetSpecialOffer()}";
+        }
+        
+public override double GetCashBack(double monthlyPurchaseAmount)
+{
+            double percentage = (monthlyPurchaseAmount > 1000.0d) ? 0.05d : 0.02d;///// ternary  <2% for <1000; % cash back for montlhy purchases >1000 
+            return currentMonthPurchases * percentage;
+}
+    }//end class
+
+}//end main 
--- shoppingApp/Program.cs
+++ shoppingApp/Program.cs
@@ -21,9 +21,11 @@ namespace ShopKo // Note: actual namespace depends on the project name.
 
         memberList.Add(new Regular (1002, "woot@aol.com", "Regular Member" , 75, 250.00, 50.23));
 
-        memberList.Add(new Executive (2001, "bread@beads.org", "Executive Member" , 150, 500.00, 100));
-        memberList.Add(new Executive (2001, "lumber@truss.biz", "Executive Member" , 300, 1000.01, 10.5));
+        memberList.Add(new Executive (2001, "bread@beads.org", "Executive Member" , 150, 500.00));
+        memberList.Add(new Executive (2002, "lumber@truss.biz", "Executive Member" , 300, 1000.01));
 
+        memberList.Add(new NonProfit (3001, "susan@daycare.edu", "Non-Profit Member" , 200, 1300.01, 10.5));
+        memberList.Add(new NonProfit (3002, "robert@church.edu", "Non-Profit Member" , 250, 1300.01, 10.5));
 
             // Print the list
             foreach (Member anMember in memberList)
