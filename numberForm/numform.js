899f56eeb0c0906cd7d880d49825b98659968bd6
Alex Batenhorst <alexfbaten@gmail.com>
2022-05-10 14:06:39 -0500

    using alex template again

--- numberForm/numform.js
+++ numberForm/numform.js
@@ -6,8 +6,15 @@ function validateANDadd()
     //build if boolean 
     if(newWord == "")
     {
-        alert("Please enter");
+        alert("Please enter a word");
         return false;
+    }
+    // validate that a 1 or 2 was entered as a number
+    else if ((theNewNumber != 1) && (theNewNumber != 2)) {
+    // a 1 or 2 was not entered as the number so tell user and clear the field
+    alert("Please enter a 1 or 2 for the list.");
+    document.forms["myForm"]["newNumber"].value = "";
+    return false;
     }
 
 
