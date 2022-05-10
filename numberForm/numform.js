16de1248eb99cc57ae35980b743fdf1ddcb04672
Alex Batenhorst <alexfbaten@gmail.com>
2022-05-10 13:33:22 -0500

    js workin, creates alert

---
+++ numberForm/numform.js
@@ -0,0 +1,21 @@
+function validateANDadd()
+{
+    //TODO: play items in box 
+    var newWord = document.forms["myForm"]["newWord"].value;
+    var newNumber = document.forms["myForm"]["newNumber"].value;
+    //build if boolean 
+    if(newWord == "")
+    {
+        alert("Please enter");
+        return false;
+    }
+
+
+
+
+
+
+
+
+
+}//end validae function 
