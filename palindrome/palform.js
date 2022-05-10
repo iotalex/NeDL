49a5223e1851304060aab18831c84ce8bcc4c134
Alex Batenhorst <alexfbaten@gmail.com>
2022-05-10 15:29:36 -0500

    come up with logic to check for palindrome

--- palindrome/palform.js
+++ palindrome/palform.js
@@ -1,10 +1,15 @@
-function validateANDadd() {
+function checkPalindrome() {
     // place the values in the form into variables
     var theNewWord = document.forms["myForm"]["newWord"].value;
+
+//find the length of a string 
+  const len = document.forms["myForm"]["newWord"].value.length
+
     var theNewNumber = document.forms["myForm"]["newNumber"].value;
     // validate that something was entered as a word
+    
+   
     if (theNewWord == "") {
-      // no word was entered so tell the user
       alert("Please enter a word to check");
       return false;
     }
@@ -15,21 +20,45 @@ function validateANDadd() {
       document.forms["myForm"]["newNumber"].value = "";
       return false;
     }
+
+
+
+
     else {
         // a word was entered and a 1 or 2 was entered as the number 
         // so add the word to the proper table 
         if (theNewNumber==1){
            var tableRef = document.getElementById("myList1");
-           (tableRef.insertRow(tableRef.rows.length)).innerHTML = theNewWord;}
+           (tableRef.insertRow(tableRef.rows.length)).innerHTML = theNewWord + ": " + checkPalindrome1(theNewWord);}
         else{
            var tableRef = document.getElementById("myList2");
-           (tableRef.insertRow(tableRef.rows.length)).innerHTML = theNewWord;}
+           (tableRef.insertRow(tableRef.rows.length)).innerHTML = theNewWord+ ": " + checkPalindrome2(theNewWord);}
         // erase the form fields
         document.forms["myForm"]["newWord"].value = "";
         document.forms["myForm"]["newNumber"].value = "";
         return true;
     }
+
+    
   }//end validate function
+
+//TODO: create a function for 1 palindrome algorithm  (bool)
+function checkPalindrome1(wordtocheck)///***stubbing it out */
+{
+  //TODO: what is logic to determin it's a palindrome  
+
+  //TODO:
+  return "in check palindrom 1 " + wordtocheck; 
+}
+
+//TODO: create a function for 2 palindrome algorithm 
+function checkPalindrome2(wordtocheck)
+{
+  //TODO:  
+
+  //TODO:
+  return "in check palindrom 2 " + wordtocheck; 
+}
 
   function clearList1() {
     // clear the table of all rows
