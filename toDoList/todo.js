6ff9e7b59820fe3e4789502c056a5fab7c837bb6
Alex Batenhorst <alexfbaten@gmail.com>
2022-05-12 13:56:09 -0500

    TypeScript working to enter into the right table

--- toDoList/todo.js
+++ toDoList/todo.js
@@ -1,6 +1,10 @@
 function AddTask() {
     var newTask = ""; //want test values
-    newTask = document.getElementById("newTask").value;
-    alert(newTask);
+    newTask = document.getElementById("newTask").value; //value going into alert
+    var tableRef = document.getElementById("myTaskList");
+    (document.getElementById("myTaskList")).innerHTML = newTask + ': ';
+    //newTask = (<HTMLInputElement>tableRef.document.forms("myTaskList")).value;
+    //alert (newTask);
+    //myTaskList is the access to the right table 
     // document.getElementById("myForm")!.innerHTML = "myForm " + newTask + " !";
 }
--- toDoList/todo.ts
+++ toDoList/todo.ts
@@ -1,6 +1,11 @@
 function AddTask() {
     let newTask : string = ""; //want test values
 
-    newTask = (<HTMLInputElement>document.getElementById("newTask")).value; alert (newTask);//value not coming in corrrectly
-   // document.getElementById("myForm")!.innerHTML = "myForm " + newTask + " !";
+    newTask = (<HTMLInputElement>document.getElementById("newTask")).value; //value going into alert
+    var tableRef = document.getElementById("myTaskList");
+           (document.getElementById("myTaskList")).innerHTML = newTask + ': ' 
+    //alert (newTask);
+
+
+  
 }
