6fd551bc6da06eef65541a43283a55abea998768
Alex Batenhorst <alexfbaten@gmail.com>
2022-05-12 14:07:13 -0500

    puedo clear list

--- toDoList/todo.js
+++ toDoList/todo.js
@@ -3,8 +3,11 @@ function AddTask() {
     newTask = document.getElementById("newTask").value; //value going into alert
     var tableRef = document.getElementById("myTaskList");
     (document.getElementById("myTaskList")).innerHTML = newTask + ': ';
-    //newTask = (<HTMLInputElement>tableRef.document.forms("myTaskList")).value;
     //alert (newTask);
-    //myTaskList is the access to the right table 
-    // document.getElementById("myForm")!.innerHTML = "myForm " + newTask + " !";
+}
+function clearTask() {
+    var newTask = ""; //want test values
+    newTask = document.getElementById("newTask").value; //value going into alert
+    var tableRef = document.getElementById("myTaskList");
+    (document.getElementById("myTaskList")).innerHTML = "";
 }
--- toDoList/todo.ts
+++ toDoList/todo.ts
@@ -1,11 +1,17 @@
 function AddTask() {
     let newTask : string = ""; //want test values
-
     newTask = (<HTMLInputElement>document.getElementById("newTask")).value; //value going into alert
     var tableRef = document.getElementById("myTaskList");
            (document.getElementById("myTaskList")).innerHTML = newTask + ': ' 
     //alert (newTask);
-
-
   
+}
+
+
+//clears the first item, not done
+function clearTask(){
+    let newTask : string = ""; //want test values
+    newTask = (<HTMLInputElement>document.getElementById("newTask")).value; //value going into alert
+    var tableRef = document.getElementById("myTaskList");
+           (document.getElementById("myTaskList")).innerHTML = "";
 }
