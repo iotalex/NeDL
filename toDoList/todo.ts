20e2e20b982f6cb1e4445921f2de07e88e2dd19e
Alex Batenhorst <alexfbaten@gmail.com>
2022-05-13 10:29:12 -0500

    iteration loop is working

--- toDoList/todo.ts
+++ toDoList/todo.ts
@@ -1,22 +1,35 @@
 
-    const myTasks: string[] = []; 
+const myTasks: string[] = [];
 function AddTask() {
-    
-    
-    let newTask : string = ""; //want test values
+
+
+    let newTask: string = ""; //want test values
     newTask = (<HTMLInputElement>document.getElementById("newTask")).value; //value attached to HTML
     myTasks.push(newTask);
     const myTableListHtml = ""; //assigning this to an empty string
-    for(let i = 0; i < 3; i++) //iteration loop builds up the list 
-    {
-        if ((myTableListHtml))
-                    {
-                        
-                    }
 
+    for (let task = 0; task < newTask.length; task++) {        //iteration loop builds up the list
+        
+        console.log("Balck statement" + task)
     }
-    document.getElementById("myTableList").innerHTML = myTableListHtml + ': '//this value is printed into table
-    
+    //console.log(newTask);
+    document.getElementById("myTableList").innerHTML = myTableListHtml + ': ' + task;//this value is printed into table
+    //chunky monkey
+
+    // for (var _i = 0, myTasks_1 = myTasks; _i < myTasks_1.length; _i++) {
+    //     var task = myTasks_1[_i];
+    //     console.log(task);
+
+    // for (let i = 0; i < myTasks.length; i++)  
+    // {
+        
+
+    // }
+    //document.getElementById("myTableList").innerHTML = myTableListHtml + ': '//this value is printed into table
+    //alert (newTask);
+    //console.log(newTask)
+
+
     //print in a list
     //add a row to the list 
 
@@ -25,16 +38,16 @@ function AddTask() {
     //var tableRef = document.getElementById("myTableList");
     //    var tableRef: HTMLTableElement = <HTMLTableElement> document.getElementById("myTable");
     // alert (newTask);
-//    var table: HTMLTableElement = document.getElementById("myTableList");
+    //    var table: HTMLTableElement = document.getElementById("myTableList");
 
 
-    
+
 }
 
 
 //clears the table of all rows
-function clearTask(){
-    
-    var tableRef = document.getElementById("myTableList");
-    tableRef.innerHTML = " ";
-}
+// function clearTask() {
+
+//     var tableRef = document.getElementById("myTableList");
+//     tableRef.innerHTML = " ";
+// }
