a7454507d0572a699e81b037f64bf24f6195b63c
Alex Batenhorst <alexfbaten@gmail.com>
2022-05-17 14:48:51 -0500

    need to print array

--- aboveAverage/Program.cs
+++ aboveAverage/Program.cs
@@ -7,7 +7,21 @@ namespace AboveAverage // Note: actual namespace depends on the project name.
         static void Main(string[] args)
         {
             
+        //store array
+        int[] myNum = {0, 0, 0};
+
+        myNum[1] = Convert.ToInt16(Console.ReadLine());
+        myNum[2] = Convert.ToInt16(Console.ReadLine());
+        myNum[3] = Convert.ToInt16(Console.ReadLine());
+
+        Console.WriteLine(myNum);
         
+        
+        
+        // calculat array; 
+        
+        
+        //add socre; find the mean, and then divide the total by the mean (number of scores greater than the mean)
         
         
         
@@ -22,5 +36,3 @@ namespace AboveAverage // Note: actual namespace depends on the project name.
 
 
 
-//store array, calculat array; 
-//add socre; find the mean, and then divide the total by the mean (number of scores greater than the mean)
