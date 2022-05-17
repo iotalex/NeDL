f71741a4a483c9c67bd9a928d13b9aa344a70b51
Alex Batenhorst <alexfbaten@gmail.com>
2022-05-17 14:19:29 -0500

    start new program - kattis

---
+++ aboveAverage/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AboveAverage // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        
+        
+        
+        
+        
+        }//end static void 
+
+    }//end class program 
+}
+
+
+
+
+
+
+//store array, calculat array; 
+//add socre; find the mean, and then divide the total by the mean (number of scores greater than the mean)
