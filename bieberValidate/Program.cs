db88414afbb16c05acdfbb1665cbcd66d0de01c7
Alex Batenhorst <alexfbaten@gmail.com>
2022-05-17 10:22:50 -0500

    creating test data

--- bieberValidate/Program.cs
+++ bieberValidate/Program.cs
@@ -6,7 +6,28 @@ namespace Ahhh // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please say open and say 'ahh....'");
+            openMouth[] newThroatArray =new openMouth[5];
+            //now load in some test data
+            newDDRSong = new openMouth("aaah");//This is creating a new openWide object
+
+            //promp user to open wide
+            Console.WriteLine("Please say open and say 'aaah....'");
+            //get input from user 
+            string numberOfConsonants = Console.ReadLine();
+
+
+
+
+
+            //validate number of a's from user
+
+            //validate 'h' from user
+
+            //bool true or false ( line input will contain between 0 an 99 a's followed by a single 'h')
+
+                //output 1 no
+
+                //out 2 go 
         
         
         
