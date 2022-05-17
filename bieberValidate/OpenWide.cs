ffb724541545c33243fed5081680939fa95008af
Alex Batenhorst <alexfbaten@gmail.com>
2022-05-17 10:41:07 -0500

    new class, enter properties

---
+++ bieberValidate/OpenWide.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ahhh // Note: actual namespace depends on the project name.
+{
+    public class OpenWide
+    {
+        
+
+
+
+    }
+}
--- bieberValidate/Program.cs
+++ bieberValidate/Program.cs
@@ -6,21 +6,27 @@ namespace Ahhh // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            openMouth[] newThroatArray =new openMouth[5];
-            //now load in some test data
-            newDDRSong = new openMouth("aaah");//This is creating a new openWide object
+            OpenWide[] newThroatArray =new openMouth[5];
+            //array of test data (3 a's)
+            OpenWide[] newOpenMouth = new openMouth("aaah", 3);//This is creating a new openWide object
 
             //promp user to open wide
             Console.WriteLine("Please say open and say 'aaah....'");
             //get input from user 
             string numberOfConsonants = Console.ReadLine();
 
+            Console.WriteLine(openMouth);
 
-
-
-
+            newOpenMouthArray[1] = newOpenMouth; //main object
+     
             //validate number of a's from user
-
+            // for(string index = 0; index < newOpenMouthArray.Length; index++)
+            //             {
+            //                 if(!(newOpenMouthArray[index]==0))
+            //                 {
+            //                     Console.WriteLine(newOpenMouthArray[index]);
+            //                 }
+            //             }
             //validate 'h' from user
 
             //bool true or false ( line input will contain between 0 an 99 a's followed by a single 'h')
