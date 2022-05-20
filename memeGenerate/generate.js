fbe702809b296646fdf6fb9a86f078d3e023ae69
Alex Batenhorst <alexfbaten@gmail.com>
2022-05-20 18:21:05 -0500

    excuses for office and college

--- memeGenerate/generate.js
+++ memeGenerate/generate.js
@@ -28,10 +28,9 @@ async function getRandomExcuse() {
     return true;
   }
 
-  //TODO: build new function 
-  async function getNumberExecuse() {
+  async function getOfficeExcuse() {
     // first build the API call string by starting with the URL
-    var apiString = "https://excuser.herokuapp.com/v1/excuse";
+    var apiString = "https://excuser.herokuapp.com/v1/excuse/office";
 
     try {
    // now make the API call to the web service using the string and store what is returned in response
@@ -50,10 +49,84 @@ async function getRandomExcuse() {
         // stringify and print out the JSON object in the RawData section
         document.getElementById("myRawData").innerHTML = JSON.stringify(jsonData);
         //and then iterate through id# that picks it up     
+        let excuseText = JSON.stringify(jsonData[0]['excuse']);                 //starting here, the new function will be different
+        //alert(jsonData.length);
+        alert(excuseText);
+        //the formated read out text without the ID# and mess etc. 
+        document.getElementById("myFormattedData").innerHTML = "<p>" + excuseText + "</p>";
+
+    return true;
+  }
+
+  async function getCollegeExcuse() {
+    // first build the API call string by starting with the URL
+    var apiString = "https://excuser.herokuapp.com/v1/excuse/college/4";
+
+    try {
+   // now make the API call to the web service using the string and store what is returned in response
+   var response = await fetch(apiString);
+    }
+    catch (error) {
+        alert(error)
+        throw error
+    }
+  
+    // finally, print the response in the various formats
+    document.getElementById("myRawData").innerHTML = "";   // clear what was previously shown
+    document.getElementById("myFormattedData").innerHTML = "";   // clear what was previously shown
+
+    var jsonData = await response.json();  // read the response as JSON
+        // stringify and print out the JSON object in the RawData section
+        document.getElementById("myRawData").innerHTML = JSON.stringify(jsonData);
+        //and then iterate through id# that picks it up     
+        let excuseText = JSON.stringify(jsonData[0]['excuse']);                 //starting here, the new function will be different
+        //alert(jsonData.length);
+        alert(excuseText);
+        //the formated read out text without the ID# and mess etc. 
+        document.getElementById("myFormattedData").innerHTML = "<p>" + excuseText + "</p>";
+
+    return true;
+  }
+
+
+
+
+
+
+  //TODO: build new function 
+  async function getNumberExecuse() {
+    // first build the API call string by starting with the URL
+    var apiString = "https://excuser.herokuapp.com/v1/excuse/family/";
+
+    try {
+   // now make the API call to the web service using the string and store what is returned in response
+   var response = await fetch(apiString);
+    }
+    catch (error) {
+        alert(error)
+        throw error
+    }
+  
+    // finally, print the response in the various formats
+    document.getElementById("myRawData").innerHTML = "";   // clear what was previously shown
+    document.getElementById("myFormattedData").innerHTML = "";   // clear what was previously shown
+
+    var jsonData = await response.json();  // read the response as JSON
+        // stringify and print out the JSON object in the RawData section
+        document.getElementById("myRawData").innerHTML = JSON.stringify(jsonData);
+
+        let excuseText = JSON.stringify(jsonData[0]['excuse']);                 //starting here, the new function will be different
+        //alert(jsonData.length);
+        alert(excuseText);
+        //the formated read out text without the ID# and mess etc. 
+        document.getElementById("myFormattedData").innerHTML = "<p>" + excuseText + "</p>";
+
+   
+        //and then iterate through id# that picks it up     
 //TODO: it has to grap numebr user (put number of elements I want to get next to the button; e.g. droplist 1-5)
 
   //TODO: spin through each item (loop) in the array
-  
+
     //TODO:  
 
     return true;
