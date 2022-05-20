af90cc7b0f4fc178bee88e7df7000f26ead8abfd
Alex Batenhorst <alexfbaten@gmail.com>
2022-05-20 18:39:37 -0500

    ready for presentation

--- memeGenerate/generate.js
+++ memeGenerate/generate.js
@@ -21,7 +21,7 @@ async function getRandomExcuse() {
         //and then iterate through id# that picks it up     
         let excuseText = JSON.stringify(jsonData[0]['excuse']);                 //starting here, the new function will be different
         //alert(jsonData.length);
-        alert(excuseText);
+        //alert(excuseText);
         //the formated read out text without the ID# and mess etc. 
         document.getElementById("myFormattedData").innerHTML = "<p>" + excuseText + "</p>";
 
@@ -50,8 +50,6 @@ async function getRandomExcuse() {
         document.getElementById("myRawData").innerHTML = JSON.stringify(jsonData);
         //and then iterate through id# that picks it up     
         let excuseText = JSON.stringify(jsonData[0]['excuse']);                 //starting here, the new function will be different
-        //alert(jsonData.length);
-        alert(excuseText);
         //the formated read out text without the ID# and mess etc. 
         document.getElementById("myFormattedData").innerHTML = "<p>" + excuseText + "</p>";
 
@@ -81,13 +79,41 @@ async function getRandomExcuse() {
         //and then iterate through id# that picks it up     
         let excuseText = JSON.stringify(jsonData[0]['excuse']);                 //starting here, the new function will be different
         //alert(jsonData.length);
-        alert(excuseText);
+        //alert(excuseText);
         //the formated read out text without the ID# and mess etc. 
         document.getElementById("myFormattedData").innerHTML = "<p>" + excuseText + "</p>";
 
     return true;
   }
+  async function getChildrenExcuse() {
+    // first build the API call string by starting with the URL
+    var apiString = "https://excuser.herokuapp.com/v1/excuse/children/";
 
+    try {
+   // now make the API call to the web service using the string and store what is returned in response
+   var response = await fetch(apiString);
+    }
+    catch (error) {
+        alert(error)
+        throw error
+    }
+  
+    // finally, print the response in the various formats
+    document.getElementById("myRawData").innerHTML = "";   // clear what was previously shown
+    document.getElementById("myFormattedData").innerHTML = "";   // clear what was previously shown
+
+    var jsonData = await response.json();  // read the response as JSON
+        // stringify and print out the JSON object in the RawData section
+        document.getElementById("myRawData").innerHTML = JSON.stringify(jsonData);
+        //and then iterate through id# that picks it up     
+        let excuseText = JSON.stringify(jsonData[0]['excuse']);                 //starting here, the new function will be different
+        //alert(jsonData.length);
+        //alert(excuseText);
+        //the formated read out text without the ID# and mess etc. 
+        document.getElementById("myFormattedData").innerHTML = "<p>" + excuseText + "</p>";
+
+    return true;
+  }
 
 
 
@@ -117,7 +143,7 @@ async function getRandomExcuse() {
 
         let excuseText = JSON.stringify(jsonData[0]['excuse']);                 //starting here, the new function will be different
         //alert(jsonData.length);
-        alert(excuseText);
+        //alert(excuseText);
         //the formated read out text without the ID# and mess etc. 
         document.getElementById("myFormattedData").innerHTML = "<p>" + excuseText + "</p>";
 
