b690dcdc6cad137f428c155d36487f4c23f627c8
Alex Batenhorst <alexfbaten@gmail.com>
2022-05-23 12:57:55 -0500

    main parent class strings

---
+++ ConsoleCalc/ConsoleCalc/Media.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCalc
+{
+    internal class Media
+    {
+        public string Name { get; set; } 
+        public string Format { get; set; }
+        public int Year { get; set; }
+
+
+        public string Media (string NewName, string NewFormat, int NewYear)//main string 
+        {
+            Name = NewName;
+            Format = NewFormat;
+            Year = NewYear;
+        }
+
+
+        
+
+
+    }
+
+
+    
+
+
+}//end name space
