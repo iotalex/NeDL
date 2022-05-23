8201d2f56cdfb978826f9e8a1b4ff45ab8097eb0
Alex Batenhorst <alexfbaten@gmail.com>
2022-05-23 16:43:04 -0500

    added TODOs for Music child class

--- ConsoleCalc/ConsoleCalc/Music.cs
+++ ConsoleCalc/ConsoleCalc/Music.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleCalc
 {
-    internal class Music : Media
+    internal class Music : Media//TODO: add a public abstract method here
     {
         public Music (string NewName, string NewFormat, int NewYear)//default main string 
         {
@@ -14,6 +14,10 @@ namespace ConsoleCalc
             Format = NewFormat;
             Year = NewYear;
         }
+        
+        //TODO: add a public abract method to add "artist" and "almbum name" for the ToString(); 5-23-22
+
+
 
         public override string ToString()
         {
