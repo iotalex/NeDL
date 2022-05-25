28fd9b42cbfad7e8faa30522c2c6639adc84f466
Alex Batenhorst <alexfbaten@gmail.com>
2022-05-25 13:41:56 -0500

    start inversion example tutorial

---
+++ InversionControl/InversionControl/FileLogger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InversionControl
+{
+    internal class ProductServices
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
