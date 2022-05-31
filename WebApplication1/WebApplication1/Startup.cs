59b09548ec7125f220f07baa29db04f34175b77d
Alex Batenhorst <alexfbaten@gmail.com>
2022-05-31 14:29:09 -0500

    going to start over

--- WebApplication1/WebApplication1/Startup.cs
+++ WebApplication1/WebApplication1/Startup.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
-using TodoApi.Models;
+using TodoApi.Models;  //using TodoApi.Models;
 
 namespace TodoApi
 {
