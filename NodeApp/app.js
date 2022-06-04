041740f47e2f5e30f42d6c46101bb263585561c9
Alex Batenhorst <alexfbaten@gmail.com>
2022-06-04 15:11:01 -0500

    get result string array

--- NodeApp/app.js
+++ NodeApp/app.js
@@ -1,6 +1,10 @@
 
 const fs = require ('fs');
 
-const files = fs.readdirSync('./');
+//const files = fs.readdirSync('./');
+//console.log(files);
 
-console.log(files);
+fs.readdir('./', function(err, files) {
+ if(err) console.log('Error', err);
+ else console.log('Result', files);
+});
