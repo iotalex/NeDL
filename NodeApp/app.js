59fd9b3ae9641c34bf7b1f6edd17147bb98edcb2
Alex Batenhorst <alexfbaten@gmail.com>
2022-06-04 16:11:32 -0500

    build tiny server using node, implt. express node

--- NodeApp/app.js
+++ NodeApp/app.js
@@ -1,12 +1,20 @@
 //https://nodejs.org/api/http.html  - for HTML module// Class:net.Server class
+//type in "http://localhost:3000/api/courses" to view the array build, it's a tiny backend server using node
 const { Socket } = require('dgram');
 const http = require('http');
 
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.write('Hello World');
+        res.end();
+    }
 
-server.on('connection', (Socket) => {
-    console.log('New connection...');
+    if (req.url === '/api/courses'){
+        res.write(JSON.stringify([1, 2, 3]))
+        res.end();
+    }
 });
+
 
 server.listen(3000);
 
