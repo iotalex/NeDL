77108d97c9ac8d063dd02c5b5fce2a75a3655948
Alex Batenhorst <alexfbaten@gmail.com>
2022-06-04 15:57:04 -0500

    logger.js/app.js/ event emitter function

--- NodeApp/app.js
+++ NodeApp/app.js
@@ -1,13 +1,11 @@
 //https://nodejs.org/api/events.html  **useful, core info for node .js functions
+const EventEmitter = require('events');
 
-const EventEmitter = require('events');  //this line is a class
-const emitter = new EventEmitter;     //emitter is an obejct 
-
+const Logger = require('./logger');
+const logger = new Logger();
 //Register a listener 
-emitter.on('messageLogged', (arg) => { //event, e, or eventarg
+logger.on('messageLogged', (arg) => { //event, e, or eventarg
     console.log('Listener called', arg);
 });
-//Raise an event 
-emitter.emit('messageLogged', {id: 1, url: 'http://'});
 
-//Raise: logging (data: message)
+logger.log('message');
--- NodeApp/logger.js
+++ NodeApp/logger.js
@@ -1,15 +1,20 @@
-
-console.log(__filename);
-console.log(__dirname);
+const EventEmitter = require('events');  //this line is a class
 
 var url = 'http://mylogger.io/log';
 
-function log(message) {
-    //Send an HTTP request 
-    console.log(message);
+    class Logger extends EventEmitter {  //class will have function define in emitter 
+        log(message) {               //ability to log a message. 
+            //Send an HTTP request 
+            console.log(message);
+            
+            //Raise an event 
+            this.emit('messageLogged', {id: 1, url: 'http://'}); //use "this" as it it exents alert for the class "Logger"
+                                                                    //and EventEmitter class 
+        }
 }
 
-module.exports = log;
+module.exports = Logger;
+
 
     
 
