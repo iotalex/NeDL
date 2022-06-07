66616e4e9d233030036abf74346cddd3a233077f
Alex Batenhorst <alexfbaten@gmail.com>
2022-06-07 09:43:48 -0500

    add properties

---
+++ WebMVP/WebMVP/Models/LogItem.cs
@@ -0,0 +1,11 @@
+﻿namespace WebMVP.Models
+{
+    public class LogItem
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public string Format { get; set; }   
+        public bool IsComplete { get; set; }
+    }
+}
