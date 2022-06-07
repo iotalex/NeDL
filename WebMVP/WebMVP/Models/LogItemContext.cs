f69f9f6947989a310c95468e17740bb1864657e5
Alex Batenhorst <alexfbaten@gmail.com>
2022-06-07 09:51:12 -0500

    registered database

---
+++ WebMVP/WebMVP/Models/LogItemContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi.Models
+{
+    public class LogItemContext : DbContext
+    {
+        public LogItemContext(DbContextOptions<LogItemContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<LogItem> LogItems { get; set; }
+    }
+}
