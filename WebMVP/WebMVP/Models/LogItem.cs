cf781b8a548bb5fb7af6bc15baf1d41eaac5065d
Alex Batenhorst <alexfbaten@gmail.com>
2022-06-07 10:56:02 -0500

    updated LogItem class, edited DTO secret class, optional

--- WebMVP/WebMVP/Models/LogItem.cs
+++ WebMVP/WebMVP/Models/LogItem.cs
@@ -8,4 +8,13 @@
         public string Format { get; set; }   
         public bool IsComplete { get; set; }
     }
+
+    public class LogItemDTO //optional secret class 
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public string Format { get; set; }
+        public bool IsComplete { get; set; }
+    }
 }
