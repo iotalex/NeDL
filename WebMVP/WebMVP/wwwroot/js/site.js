d3a99f649460b41efd7e86ec5c8cebb43f32370d
Alex Batenhorst <alexfbaten@gmail.com>
2022-06-08 13:11:13 -0500

    enter format lives, working

--- WebMVP/WebMVP/wwwroot/js/site.js
+++ WebMVP/WebMVP/wwwroot/js/site.js
@@ -32,6 +32,32 @@ function addItem() {
         .catch(error => console.error('Unable to add item.', error));
 }
 
+/*attempt formation function */
+function addItem() {
+    const addNameTextbox = document.getElementById('add-format');
+
+    const item = {
+        isComplete: false,
+        name: addNameTextbox.value.trim()
+    };
+
+    fetch(uri, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(item)
+    })
+        .then(response => response.json())
+        .then(() => {
+            getItems();
+            addNameTextbox.value = '';
+        })
+        .catch(error => console.error('Unable to add item.', error));
+}
+
+
 function deleteItem(id) {
     fetch(`${uri}/${id}`, {
         method: 'DELETE'
@@ -43,7 +69,7 @@ function deleteItem(id) {
 function displayEditForm(id) {
     const item = todos.find(item => item.id === id);
 
-    document.getElementById('edit-title').value = item.title;
+    document.getElementById('edit-name').value = item.name;
     document.getElementById('edit-id').value = item.id;
     document.getElementById('edit-isComplete').checked = item.isComplete;
     document.getElementById('editForm').style.display = 'block';
@@ -92,10 +118,11 @@ function _displayItems(data) {
     const button = document.createElement('button');
 
     data.forEach(item => {
-        let isCompleteCheckbox = document.createElement('input');
+        let isCompleteCheckbox = document.createElement('input'); //where first text input lives
         isCompleteCheckbox.type = 'checkbox';
         isCompleteCheckbox.disabled = true;
         isCompleteCheckbox.checked = item.isComplete;
+
 
         let editButton = button.cloneNode(false);
         editButton.innerText = 'Edit';
@@ -108,9 +135,10 @@ function _displayItems(data) {
         let tr = tBody.insertRow();
 
         let td1 = tr.insertCell(0);
-        td1.appendChild(isCompleteCheckbox);
+        /*let textNode = document.createTextNode(item.format);*/
+        /*td1.appendChild(textNode);*/
 
-        let td2 = tr.insertCell(1);
+        let td2 = tr.insertCell(1);             //enter "format" lives
         let textNode = document.createTextNode(item.name);
         td2.appendChild(textNode);
 
@@ -119,6 +147,10 @@ function _displayItems(data) {
 
         let td4 = tr.insertCell(3);
         td4.appendChild(deleteButton);
+
+        /*let td5 = tr.insertCell(4);     //where format lives
+        let textNode = document.createTextNode(item.format);
+        td5.appendChild(textNode);*/
     });
 
     todos = data;
