7ccb0c7c413459ef92384c049a4f22e9bd2357e6
Alex Batenhorst <alexfbaten@gmail.com>
2022-06-20 11:26:24 -0500

    got app. working

--- ShoppingListAngular/src/app/app-routing.module.ts
+++ ShoppingListAngular/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MyListComponent } from './my-list/my-list.component';
 
 const routes: Routes = [{ path: '', component: MyListComponent }];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-
