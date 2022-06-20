4cd5f56ed9b2a66db65de17bea9eb86fda2911d1
Alex Batenhorst <alexfbaten@gmail.com>
2022-06-20 10:43:08 -0500

    added const routes to app-routing.module

--- ShoppingListAngular/ShoppingListAngular/src/app/app-routing.module.ts
+++ ShoppingListAngular/ShoppingListAngular/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: MyListComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
